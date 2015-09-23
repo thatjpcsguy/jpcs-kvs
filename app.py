a79462d485a6bc0f82d026387ed084c1fb7d5e62
James Peter Cooper-Stanbury <jpcs@freelancer.com>
2015-09-23 16:11:41 +1000

    updated to make it more generic

--- app.py
+++ app.py
@@ -5,17 +5,22 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/project/<project_id>', methods=['GET', 'POST'])
-def project(project_id):
+@app.route('/<endpoint>/<key_id>', methods=['GET', 'POST'])
+def project(endpoint, key_id):
+    directory = "data/" + endpoint
+    filename = directory + "/" + key_id + ".p"
+
     if request.method == 'POST':
+        if not os.path.exists(directory):
+            os.makedirs(directory)
         obj = json.loads(request.form['data'])
-        pickle.dump(obj, open("data/" + project_id + ".p", "wb"))
+        pickle.dump(obj, open(filename, "wb"))
 	return "success"
     elif request.method == 'GET':
-	if os.path.isfile("data/" + project_id + ".p"):
-            return json.dumps(pickle.load(open("data/" + project_id + ".p", "rW")))
+	if os.path.isfile(filename):
+            return json.dumps(pickle.load(open(filename, "rW")))
         else:
-            return json.dumps([])
+            return json.dumps([None])
 
 
 if __name__ == "__main__":
